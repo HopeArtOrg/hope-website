1e7c524543ea4df32650981e0a31ef7cab269800
Noah Trần <trananhquan1009@gmail.com>
2026-03-05 18:34:10 +0700

    feat: antfu linter / husky pre-commit / svelte (#4)

--- astro.config.ts
+++ astro.config.ts
@@ -1,4 +1,11 @@
+import svelte from "@astrojs/svelte";
 // @ts-check
 import { defineConfig } from "astro/config";
 
-export default defineConfig({});
+export default defineConfig({
+  integrations: [svelte()],
+
+  server: {
+    port: 3000,
+  },
+});
