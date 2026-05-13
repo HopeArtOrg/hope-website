a3329111e04975ec35af94033e207f369c8df712
Noah Trần <trananhquan1009@gmail.com>
2026-05-14 01:33:56 +0700

    fix(hero-section): dropdown menu action button (#40)
    * fix(hero-section): add key directive to each block in download-actions
    
    * fix(hero-section): use unique platform href for each key
    
    * style(hero-section): update hero section components
    
    * fix(ui): unique id for block to properly render
    
    * fix(ui): revert the hero-section.svelte

--- src/components/landing/hero-section/download-actions.svelte
+++ src/components/landing/hero-section/download-actions.svelte
@@ -94,7 +94,7 @@
         {/snippet}
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end">
-        {#each altPlatforms as platform, i (platform.name)}
+        {#each altPlatforms as platform, i (platform.href)}
           {#if i > 0}
             <DropdownMenuSeparator />
           {/if}
