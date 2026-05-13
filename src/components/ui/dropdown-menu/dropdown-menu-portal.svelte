6ba4519ea1d3ce4cbabdc31323e645f5d59014ba
Coder-Blue <trananhquan1009@gmail.com>
2026-05-14 00:22:24 +0700

    fix(dropdown-menu): remove unnecessary portal wrapper that was blocking content rendering

--- src/components/ui/dropdown-menu/dropdown-menu-portal.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-  import { DropdownMenu as DropdownMenuPrimitive } from "bits-ui";
-
-  const { children, ...restProps }: DropdownMenuPrimitive.PortalProps = $props();
-</script>
-
-<DropdownMenuPrimitive.Portal {...restProps}>
-  {@render children?.()}
-</DropdownMenuPrimitive.Portal>
